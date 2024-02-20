4de170f531ab0309dd893503c882f84af05b70db
prashanth <reddyprashanth512@gmail.com>
2024-02-20 06:19:24 +0000

    solution

---
+++ src/components/MemoryMatrix/index.js
@@ -0,0 +1,14 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class MemoryMatrix extends Component {
+  render() {
+    return (
+      <div>
+        <h1>shiva paravathi</h1>
+      </div>
+    )
+  }
+}
+export default MemoryMatrix
